b43c8c8f6db0694bf3adb2004aab31fbdb4f13aa
Willian dos Santos Souza <willian.santos.souza@live.com>
2021-02-27 19:48:12 -0300

    feat: Finish toggle dark mode

--- src/components/ToogleSwitchDarkMode.tsx
+++ src/components/ToogleSwitchDarkMode.tsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import  DarkModeToggle from 'react-dark-mode-toggle'
+import useDarkMode from 'use-dark-mode'
 import styles from '../styles/components/ToogleSwitchDarkMode.module.css'
 
 
 export function ToggleSwitchDarkMode() {
-    const [checked, setChecked] = useState(false)
+    const [isDarkMode, setIsDarkMode] = useState(()=>false)
+    const darkMode = useDarkMode(false)
+    
 
+
+    useEffect(()=>{
+        darkMode.toggle()
+    },[isDarkMode])
+    
 
     return (
         <label className={styles.toogleDarkMode}>
-            <DarkModeToggle checked={checked} onChange={(e) => { setChecked(e) }} size={60}/>
+            <DarkModeToggle 
+            onChange={setIsDarkMode}
+            checked={isDarkMode}
+            size={60}/>
         </label>
     )
+
+    
 }
